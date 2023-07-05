77a72648685b37d838007d00cbe8605e08e8b0ac
Mohammad Termzee <mh.termzee@gitlab.com>
2023-07-05 16:27:26 +0200

    change format

--- src/store/todoList.js
+++ src/store/todoList.js
@@ -1,34 +1,34 @@
 import { defineStore } from "pinia";
 
 export const useTodoListStore = defineStore("todoList", {
-  // state
-  state: () => ({
-    todoList: [],
-    id: 0,
-  }),
-  // actions
-  actions: {
-    addTodoItem(item) {
-      this.todoList.push({
-        item,
-        id: this.id++,
-        completed: false,
-      });
-    },
-    removeTodoItem(itemId) {
-      this.todoList = this.todoList.filter((item) => item.id !== itemId);
-    },
-    toggleCompleted(idToFind) {
-      const todo = this.todoList.find((obj) => obj.id === idToFind);
-      if (todo) {
-        todo.completed = !todo.completed;
-      }
-    },
-  },
-  // getters
-  getters: {
-    getTodoList() {
-      return this.todoList;
-    },
-  },
+	// state
+	state: () => ({
+		todoList: [],
+		id: 0,
+	}),
+	// actions
+	actions: {
+		addTodoItem(item) {
+			this.todoList.push({
+				item,
+				id: this.id++,
+				completed: false,
+			});
+		},
+		removeTodoItem(itemId) {
+			this.todoList = this.todoList.filter((item) => item.id !== itemId);
+		},
+		toggleCompleted(idToFind) {
+			const todo = this.todoList.find((obj) => obj.id === idToFind);
+			if (todo) {
+				todo.completed = !todo.completed;
+			}
+		},
+	},
+	// getters
+	getters: {
+		getTodoList() {
+			return this.todoList;
+		},
+	},
 });
