034bbcce019a53bf8805356d9d875be9a6a1e76f
Mohammad Termzee <mh.termzee@gitlab.com>
2023-07-05 14:16:33 +0200

    add pinia instance

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { createPinia } from 'pinia';
 
-createApp(App).use(store).use(router).mount("#app");
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(store).use(router).use(pinia).mount("#app");
